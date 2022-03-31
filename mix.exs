4942f06d1be13f5296c07d6938d66f4c84201d95
Jefferson Farias <jefferson.farias7@gmail.com>
2022-03-31 08:37:27 -0300

    adding hackney adapter lib

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule GithubInfo.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tesla, "~> 1.4"}
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
